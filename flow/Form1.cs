a87a62698724eb4f2c07d210f31156c82ea396fe
Dan <66948931+Danicus2000000@users.noreply.github.com>
2024-11-14 23:26:10 +0000

    Migrated to NET 9 and removed warnings and errors

--- flow/Form1.cs
+++ flow/Form1.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace flow
+﻿namespace flow
 {
     public partial class Form1 : Form
     {
@@ -22,9 +12,11 @@ namespace flow
         {
             for (int i = 0; i < 100; i++)
             {
-                Button d = new Button();
-                d.Name = "eg" + i + "_btn";
-                d.Text = i.ToString();
+                Button d = new()
+                {
+                    Name = "eg" + i + "_btn",
+                    Text = i.ToString()
+                };
                 flowLayoutPanel1.Controls.Add(d);
             }
         }
